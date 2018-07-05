c9045e754b61931584fe3c65b2483a69ab8df62c
DESKTOP-SHJRQ9O\Puspanjali <sunwalsandesh@gmail.com>
2018-07-05 02:12:19 -0500

    test

---
+++ src/test/java/example/NewTest.java
@@ -0,0 +1,29 @@
+package example;
+
+import junit.framework.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class NewTest {
+    private WebDriver driver;
+
+    @Test
+    public void testEasy() {
+        driver.get("http://demo.guru99.com/test/guru99home/");
+        String title = driver.getTitle();
+        Assert.assertTrue(title.contains("Demo Guru99 Page"));
+    }
+
+    @BeforeTest
+    public void beforeTest() {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
+    }
+}
